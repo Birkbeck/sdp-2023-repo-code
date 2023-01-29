92cfc0d1d454c8a2635f3d29a95bfb5675f3f0c7
kontchakov <roman@dcs.bbk.ac.uk>
2023-01-29 13:15:39 +0000

    motivation for inner and local classes

--- week2/src/classes/LocalClassDemo.java
+++ week2/src/classes/LocalClassDemo.java
@@ -5,7 +5,16 @@ import java.util.Random;
 public class LocalClassDemo {
     private static final Random generator = new Random();
 
+    public static IntSequence randomIntsWithStandaloneRandomIntSequence(int low, int high) {
+        return new StandaloneRandomIntSequence(low, high, generator);
+    }
+
+    // low and high are effectively final
     public static IntSequence randomInts(int low, int high) {
+        // local and inner classes have direct access to local and instance variables
+        // thus saving on boilerplate code present in e.g., StandaloneRandomIntSequence
+        // (the boilerplate code in the constructor copies low, high and generator
+        // into instance variables of the class)
         class RandomSequence implements IntSequence {
             public int next() {
                 return low + generator.nextInt(high - low + 1);
